a5de02d971565c0c61f66dfdb8cbf1d00525906a
Ross <30704653+RossHS@users.noreply.github.com>
2024-02-23 19:42:18 +0300

    fix all lint warnings (#4)

--- lib/src/widgets/shake_jump_animation.dart
+++ lib/src/widgets/shake_jump_animation.dart
@@ -1,6 +1,5 @@
 import 'dart:math' as math;
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
