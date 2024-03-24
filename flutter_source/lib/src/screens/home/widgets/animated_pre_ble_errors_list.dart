2d39330df3c2d12f35d9ba36f0e167335dbe920c
Ross Khapilov <rossxpanteley@gmail.com>
2024-03-24 14:17:28 +0300

    Reworked AnimatedPreBLEErrorsList

--- flutter_source/lib/src/screens/home/widgets/animated_pre_ble_errors_list.dart
+++ flutter_source/lib/src/screens/home/widgets/animated_pre_ble_errors_list.dart
@@ -1,10 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:get_it/get_it.dart';
 import 'package:light_house/main.dart' as main;
 import 'package:light_house/src/controllers/ble_core/ble_controllers.dart';
 import 'package:light_house/src/widgets/errors_widgets/errors_widgets.dart';
+import 'package:mobx/mobx.dart';
 
 const _padding = EdgeInsets.all(8.0);
 
@@ -13,6 +15,7 @@ const _padding = EdgeInsets.all(8.0);
 /// Тут мы проверяем следующее
 /// 1. Наличие bluetooth на устройстве и наличие всех разрешений для работы
 /// 2. Найден ли необходимый BLE для подключения
+/// TODO 24.03.2024 - п.с. к сожалению вышел не самый удачный и красивый код, но пока оставим так, возможно потом поменяем (нет 🤡)
 class AnimatedPreBLEErrorsList extends StatefulWidget {
   const AnimatedPreBLEErrorsList({super.key});
 
@@ -21,7 +24,81 @@ class AnimatedPreBLEErrorsList extends StatefulWidget {
 }
 
 class _AnimatedPreBLEErrorsListState extends State<AnimatedPreBLEErrorsList> {
+  late StreamSubscription<BleStatus> _bleStatusListener;
+  late ReactionDisposer _react;
+
+  Widget _bleStatusWidget = const SizedBox.shrink(key: ValueKey('NON BLE STATUS INFO'));
+  Widget _bleDeviceInfoWidget = const SizedBox.shrink(key: ValueKey('NON BLE DEVICE INFO'));
   final List<Widget> _additionsErrors = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Да, не самый красивый и понятный код, но все из-за ограничений при работе с [ErrorsAnimatedList]
+    // Т.е. у нас в верхнем уровне обязательно должен быть именно конечный виджет для отрисовки,
+    // таким образом, если добавить в само дерево [StreamBuilder], то у нас поломается анимация добавления и удаления
+    _bleStatusListener = FlutterReactiveBle().statusStream.listen((event) {
+      if (!mounted) return;
+      setState(() {
+        _bleStatusWidget = switch (event) {
+          BleStatus.unsupported =>
+            const _ButtonShortHand(key: ValueKey('BLE STATUS INFO'), child: Text('BLE не поддерживается')),
+          BleStatus.poweredOff =>
+            const _ButtonShortHand(key: ValueKey('BLE STATUS INFO'), child: Text('Включите Bluetooth')),
+          BleStatus.unauthorized => const _ButtonShortHand(
+              key: ValueKey('BLE STATUS INFO'),
+              onPressed: main.permissionRequest,
+              child: Text('Запросить разрешения?'),
+            ),
+          _ => const SizedBox.shrink(key: ValueKey('NON BLE STATUS INFO')),
+        };
+      });
+    });
+    _react = reaction(
+      (_) => GetIt.I<BLEDevicePresetsInitController>().bleDeviceDataForConnection,
+      fireImmediately: true,
+      (dataForConnection) {
+        if (!mounted) return;
+
+        if (dataForConnection.value != null) {
+          setState(() {
+            _bleDeviceInfoWidget = const SizedBox.shrink(
+              key: ValueKey('NON BLE DEVICE INFO'),
+            );
+          });
+          return;
+        }
+        final controller = GetIt.I<BLEDevicePresetsInitController>();
+        setState(() {
+          _bleDeviceInfoWidget = dataForConnection.map(
+            value: (value) => _ButtonShortHand(
+              key: const ValueKey('BLE DEVICE INFO NON DATA'),
+              onPressed: controller.initBleSettings,
+              child: const Text('Нет данных для подключения'),
+            ),
+            error: (error) => _ButtonShortHand(
+              key: const ValueKey('BLE DEVICE INFO ERROR'),
+              onPressed: controller.initBleSettings,
+              child: Text(error.error!.errorMessage),
+            ),
+            loading: (loading) => const _ButtonShortHand(
+              key: ValueKey('BLE DEVICE INFO LOADING'),
+              onPressed: null,
+              child: Text('Поиск данных!'),
+            ),
+          );
+        });
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _bleStatusListener.cancel();
+    _react();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +108,8 @@ class _AnimatedPreBLEErrorsListState extends State<AnimatedPreBLEErrorsList> {
       child: RepaintBoundary(
         child: ErrorsAnimatedList(
           children: [
-            const _BLEStatusInfo(key: ValueKey('BLE STATUS INFO')),
-            // if (dataForConnectionWidget != null) dataForConnectionWidget,
-            const _BLEDevicePresetsInitInfo(key: ValueKey('BLE DEVICE INFO')),
+            _bleStatusWidget,
+            _bleDeviceInfoWidget,
             ..._additionsErrors,
           ],
         ),
@@ -42,62 +118,8 @@ class _AnimatedPreBLEErrorsListState extends State<AnimatedPreBLEErrorsList> {
   }
 }
 
-class _BLEStatusInfo extends StatelessWidget {
-  const _BLEStatusInfo({required super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: FlutterReactiveBle().statusStream,
-      builder: (context, s) {
-        if (!s.hasData) return const SizedBox.shrink();
-        return switch (s.requireData) {
-          BleStatus.unsupported => const _ButtonShortHand(child: Text('BLE не поддерживается')),
-          BleStatus.poweredOff => const _ButtonShortHand(child: Text('Включите Bluetooth')),
-          BleStatus.unauthorized => const _ButtonShortHand(
-              onPressed: main.permissionRequest,
-              child: Text('Запросить разрешения?'),
-            ),
-          _ => const SizedBox.shrink(),
-        };
-      },
-    );
-  }
-}
-
-/// Вывод информации и целевых действий под поиск девайса и предустановки данных для его подключения
-class _BLEDevicePresetsInitInfo extends StatelessWidget {
-  const _BLEDevicePresetsInitInfo({required super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Observer(
-      builder: (context) {
-        final controller = GetIt.I<BLEDevicePresetsInitController>();
-
-        final dataForConnection = controller.bleDeviceDataForConnection;
-        if (dataForConnection.value != null) return const SizedBox();
-
-        return dataForConnection.map(
-          value: (value) => _ButtonShortHand(
-            onPressed: controller.initBleSettings,
-            child: const Text('Нет данных для подключения'),
-          ),
-          error: (error) => _ButtonShortHand(
-            onPressed: controller.initBleSettings,
-            child: Text(error.error!.errorMessage),
-          ),
-          loading: (loading) => const _ButtonShortHand(
-            onPressed: null,
-            child: Text('Поиск данных!'),
-          ),
-        );
-      },
-    );
-  }
-}
-
-/// Вспомогательный виджет, дабы подогнать контент до нужный размеров
+/// Вспомогательный виджет, дабы подогнать контент до нужный размеров.
+/// Опять же, вынес логику в отдельный виджет, дабы не повторяться
 class _ErrorsSizeConstrains extends StatelessWidget {
   const _ErrorsSizeConstrains.text({
     super.key,
