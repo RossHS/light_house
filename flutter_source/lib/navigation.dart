e9abcc5641b67919cd163106b142a51d3d89a8be
Ross <30704653+RossHS@users.noreply.github.com>
2024-03-31 18:20:52 +0300

    Navigation (#21)
    * fix error with async share logs
    
    * add go_router and rework navigation
    
    * Added an option with a "triangle" transition
    
    * Added an option with a "fan" transition
    
    * switching screen transition type
    
    * Added an option with a "circle" transition

---
+++ flutter_source/lib/navigation.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:light_house/src/screens/home/home_screen.dart';
+import 'package:light_house/src/screens/logs/logs_screen.dart';
+import 'package:light_house/src/widgets/transitions/screen_transitions.dart';
+
+/// Конфигурация всей навигации приложения через go_router.
+final GoRouter router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      pageBuilder: (context, state) => _buildDefaultTransition(context, state, const HomeScreen()),
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'logs_screen',
+          pageBuilder: (context, state) => _buildDefaultTransition(context, state, const LogsScreen()),
+        ),
+      ],
+    ),
+  ],
+);
+
+/// Так как в go_router пока нельзя задать единый стиль перехода по экранам, то приходится писать вот такое вот 🤡
+CustomTransitionPage _buildDefaultTransition<T>(BuildContext context, GoRouterState state, Widget screen) {
+  return CustomTransitionPage<T>(
+    key: state.pageKey,
+    transitionDuration: const Duration(milliseconds: 700),
+    reverseTransitionDuration: const Duration(milliseconds: 700),
+    transitionsBuilder: (_, animation, __, child) => CustomClipTransition(
+      animation: CurvedAnimation(parent: animation, curve: Curves.easeInOutCirc),
+      child: child,
+    ),
+    child: screen,
+  );
+}
--- flutter_source/lib/src/utils/sp_keys.dart
+++ flutter_source/lib/src/utils/sp_keys.dart
@@ -2,3 +2,4 @@ const themeModeKey = 'ThemeMode';
 const bleDeviceIdKey = 'bleDeviceId';
 const bleServiceIdKey = 'bleServiceId';
 const settingsGlitchOnKey = 'settings_glitchOn';
+const settingsClipperKey = 'settings_clipper';
